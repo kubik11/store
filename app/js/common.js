7d809ec75c1af151721e08d92c14d9d491654a18
Herr-Gunter <kubyshkin1990@mail.ru>
2017-06-06 13:47:44 +0300

    I havnt done a list

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,11 @@ $(function() {
 		$('.list-item').each(function(item){
 			$(this).click(function(){
 			$('.list-item').removeClass('active-goods-list');
+			$('.list-item img').removeClass('disImg');
 			$(this).addClass('active-goods-list');
+			$(this).children().children().addClass('disImg');
+			event();
+
 			});
 		});
 			//Переключение посредствот стрелок
@@ -17,54 +21,68 @@ $(function() {
 				myArr.forEach(function(elem){
 						copyMyArr.push(elem);
 				});
-				
-				down.addEventListener('click', function(){
-					var cher = document.querySelector('.fa-chevron-up');
-					for(var i = 0; i < copyMyArr.length; i++){
 
-						if(copyMyArr[i].classList.contains('active-goods-list')){
+				var cher = document.querySelector('.fa-chevron-up');
+				var cherDown = document.querySelector('#ziga'); 
+				//cherDown.style.color = '#d0d0d0';
+				down.addEventListener('click', function(){
+					for(var i = 0; i < copyMyArr.length; i++){
+						if(copyMyArr[i].classList.contains('active-goods-list') && i < copyMyArr.length -1){
 							copyMyArr[i+1].classList.add('active-goods-list');
 							copyMyArr[i].classList.remove('active-goods-list');
 							break;		
 						} 
 					}	
-					for(var j = 0; j < copyMyArr.length; j++){
-						if(copyMyArr[j].classList.contains('active-goods-list') && j == copyMyArr.length - 1){
-							cher.style.color = '#f68235';
-						}
-						else{
-							cher.style.color = '#d0d0d0';
-						}
-					}
-
+					setPicture();
+					event();
+					
 				});
 				up.addEventListener('click', function(){
-					var cher1 = document.querySelector('.fa-chevron-down'); 
 					for(var i = 0; i < copyMyArr.length; i++){
-						if(copyMyArr[i].classList.contains('active-goods-list')){
+						if(copyMyArr[i].classList.contains('active-goods-list') && i > 0){
 							copyMyArr[i-1].classList.add('active-goods-list');
 							copyMyArr[i].classList.remove('active-goods-list');
 							break;
 						}
 					}	
-
-					for(var a = 0; a < copyMyArr.length; a++){
-						if(copyMyArr[a].classList.contains('active-goods-list') && a == 0){
-							cher1.style.color = '#f68235';
+					setPicture();
+					event();
+					
+				});	
+					
+				function event(){		
+					for(var i = 0; i < copyMyArr.length; i++){
+						if(copyMyArr[i].classList.contains('active-goods-list') && i == 0){
+							cherDown.style.color = '#f68235';
+							//colorF();
 						}
 						else{
-							cher1.style.color = '#d0d0d0';
+							cherDown.style.color = '#d0d0d0';
+						}
+						 if(copyMyArr[i].classList.contains('active-goods-list') && i == copyMyArr.length - 1){
+							cher.style.color = '#f68235';
+						}
+						else{
+							cher.style.color = '#d0d0d0';	
 						}
 					}
-				});	
+				}
 
 
-				function event(){
+				function setPicture(){
+					var mainPic = document.querySelector('.mainPicture');
 					for(var i = 0; i < copyMyArr.length; i++){
-						
+						var current = copyMyArr[i].classList.contains('active-goods-list');
+						if(current){
+							var currentAttribute = current.lastChild.getAttribute('src');
+							mainPic.setAttribute('src', currentAttribute);
+						}
 					}
 				}
-				
+				//function colorF(){
+				//	cherDown.style.color = '#f68235';
+				//	alert();
+				//}
 				
 });			
 			
